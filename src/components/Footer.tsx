1d2b165f5c1b05766ae7cdc404b1ebafab31391e
Lucas Panfieti <lucaspanfieti@gmail.com>
2025-09-01 18:11:42 -0300

    ajustando componentes

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer id="direitos" className="p-6 bg-gray-950 text-center">
+      {/* Direitos reservados */}
+      <p className="text-gray-500 ">
+        © 2025 Lucas Panfieti. Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+}
